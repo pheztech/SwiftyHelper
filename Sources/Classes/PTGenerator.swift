af68379e55869408317f275396ff91c0ad821ad9
Ken <klpham1016@gmail.com>
2020-05-12 22:36:17 -0400

    Secure Generator
    - Added secure random string generator

--- Sources/Classes/PTGenerator.swift
+++ Sources/Classes/PTGenerator.swift
@@ -13,4 +13,18 @@ public class PTGenerator: NSObject {
         let alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         return String((0..<4).map{ _ in alphanum.randomElement()! })
     }
+	
+	public static func secRandomString (ofLength length: Int) -> String? {
+		var data = Data(count: length)
+		
+		let result: Int32 = data.withUnsafeMutableBytes { buffer in
+			guard let pointer = buffer.bindMemory(to: UInt8.self).baseAddress else { return errSecMemoryError }
+			return SecRandomCopyBytes(kSecRandomDefault, length, pointer)
+		}
+		
+		if result == errSecSuccess {
+			return data.base64EncodedString()
+		}
+		return nil
+	}
 }
