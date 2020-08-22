5c979fa606c224e2c42b2851c16c5c3ae691601b
Ken <klpham1016@gmail.com>
2020-08-22 14:22:25 -0400

    Add more numerical conversions

--- Sources/SwiftyHelper/Extensions/Numeric+SwiftyHelper.swift
+++ Sources/SwiftyHelper/Extensions/Numeric+SwiftyHelper.swift
@@ -8,14 +8,39 @@
 
 import Foundation
 
+// MARK: - Integer
 public extension Int {
 	var double: Double {
 		Double(self)
 	}
+	
+	var int16: Int16 {
+		Int16(self)
+	}
+	
+	var cgFloat: CGFloat {
+		CGFloat(self)
+	}
 }
 
+// MARK: - Double
 public extension Double {
 	var number: NSNumber {
 		NSNumber(value: self)
+	}
+	
+	var int: Int {
+		Int(self)
+	}
+	
+	var cgFloat: CGFloat {
+		CGFloat(self)
+	}
+}
+
+// MARK: - CGFloat
+public extension CGFloat {
+	var int: Int {
+		Int(self)
 	}
 }
